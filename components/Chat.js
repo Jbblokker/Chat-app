e26f3458d1d2277175c22d64227ccb37f9b58f72
Jbblokker <jbblokker@outlook.com>
2021-08-21 01:48:20 -0500

    section 2.3

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
+//gifted chat library
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+
+//firebase 
+const firebase = require('firebase/app').default;
+require ('firebase/firestore');
 
 export default class Screen2 extends React.Component {
     constructor() {
         super();
+        // firebase configuration code
+        const firebaseConfig = {
+            apiKey: "AIzaSyB7A_zlpotJmHqPJrfHxcnEjcMyf3i7J-4",
+            authDomain: "chat-app-4d3c3.firebaseapp.com",
+            projectId: "chat-app-4d3c3",
+            storageBucket: "chat-app-4d3c3.appspot.com",
+            messagingSenderId: "632205781707",
+            appId: "1:632205781707:web:1773a5ddb7353954d5c94e",
+            measurementId: "G-5BHLFJCSQ0"
+        };
+        
+        if (!firebase.apps.length){
+            firebase.initializeApp(firebaseConfig)
+        };
+        this.referenceChatMessages = firebase.firestore().collection("messages");
         this.state = {
             messages: [],
-        }
+            user: {
+                _id:"",
+                name:"",
+                avatar:"",
+            }
+
+        };
     }
 // adding component for making message from state
     componentDidMount = () => {
        const { name } = this.props.route.params
        this.props.navigation.setOptions({ title: name});
+       this.referenceMessages = firebase.firestore().collection('messages');
+       this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+        if (!user) {
+          firebase.auth().signInAnonymously();
+        }
+       this.unsubscribe = this. referenceMessages.onSnapshot(this.onCollectionUpdated) 
         this.setState({
-            messages: [
-                {
                     _id: 1,
                     text:`Hello ${name}`,
                     createdAt: new Date(),
@@ -24,17 +54,54 @@ export default class Screen2 extends React.Component {
                         name: 'React Native',
                         avatar: 'https://placeimg.com/140/140/any',
                     },
-                        
-                },
-                {
-                    _id: 2,
-                    text: 'This is a system message' ,
-                    createdAt: new Date(),
-                    system: true,
-                },
-            ],
+                    messages: [],
         });
+        this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+    };
+
+    componentWillUnmount() {
+        this.unsubscribe();
     }
+
+    addMessage() {
+        const message = this.state.messages[0];
+        this.referenceChatMessages.add({
+          _id: message._id,
+          createdAt: message.createdAt,
+          text: message.text || null,
+          user: message.user,
+          image: message.image || null,
+          location: message.location || null
+        });
+      }
+
+    onCollectionUpdate = (querySnapshot) => {
+        const messages = [];
+        // go through each document
+        querySnapshot.forEach((doc) => {
+          // get the QueryDocumentSnapshot's data
+          const data = doc.data();
+          lists.push({
+            _id: data._id,
+            text: data.text || null,
+            CreatedAt: Data.createdAt.toDate(),
+            user: {
+                _id: data._id,
+                name: data.user.name,
+                avatar: data.user.avatar,
+            },
+            image: data.image || '',
+            location: data.location || null,
+          });
+        });
+        this.setState({
+          messages,
+        });
+      };   
+  
 // allowing user to send messages 
     onSend(messages = []) {
         this.setState((previousState) => ({
@@ -57,8 +124,7 @@ export default class Screen2 extends React.Component {
 
     }
 
-
-
+    
     render() {
          const { changeColor } = this.props.route.params
         return (
